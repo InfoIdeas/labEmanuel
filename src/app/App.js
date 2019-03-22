485839ee3914466e7b3a84bf5d13bc9ad72d80d5
Victor Hugo <vramh.developer@gmail.com>
2019-03-22 18:50:07 -0300

    actualizando app para generar la vista

--- src/app/App.js
+++ src/app/App.js
@@ -9,7 +9,33 @@ class App extends Component{
                         <a className="brand-logo" href="/">Laboratorio Clínico</a>
                     </div>
                 </nav>
-            </div>     
+                <div className="container">
+                    <div className="row">
+                        <div className="col s5">
+                            <div className="card">
+                                <div className="card-content">
+                                <form>
+                                    <div className="row">
+                                        <div className="input-field col s12">
+                                            <input type="text" placeholder="Nombre Paciente" />
+                                        </div>
+                                    </div>
+                                    <div className="row">
+                                        <div className="input-field col s12">
+                                            <textarea placeholder="Resultado CLinico" className="materialize-textarea"></textarea>
+                                        </div>
+                                    </div>
+                                    <button type="submit" className="btn btn-blue darken-4">
+                                    Enviar 
+                                    </button>
+                                </form>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col s7"></div>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
